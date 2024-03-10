d01d62809b91483f24915cb4e5f5a3ea0545b5b8
Paolo Oranges <paolo.oranges@gmail.com>
2024-03-10 20:52:38 +0100

    login auth

---
+++ main.py
@@ -0,0 +1,125 @@
+
+import os
+from googleapiclient.discovery import build
+
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow, Flow
+
+from flask import Flask, redirect, url_for, request, session
+
+
+from flask_session import Session               ## pip install Flask-Session
+import google.oauth2.credentials                ## Used by Google OAuth
+import google_auth_oauthlib.flow                ## Used by Google OAuth
+from googleapiclient.discovery import build     ## Used by Google OAuth
+
+SCOPES = ['https://www.googleapis.com/auth/userinfo.email',
+        'https://www.googleapis.com/auth/userinfo.profile',
+        "openid"]
+
+# SCOPES = 'https://www.googleapis.com/auth/photoslibrary.readonly'
+CLIENT_SECRETS_FILE = 'client_secrets.json'
+
+app = Flask(__name__)
+#app.config['SECRET_KEY'] = 'esdirolftjg rdsklthjrm,gme jkm2mw,3 werkj hswedf kdsjkh'
+app.config['SESSION_TYPE'] = "filesystem"
+app.config['SESSION_FILE_DIR'] = os.path.join(app.root_path, "sessions")
+app.config['SESSION_FILE_THRESHOLD'] = 1000
+Session(app)
+
+@app.route("/")
+def main():
+    return redirect("home")
+    
+@app.route("/home")
+def home():
+    if "credentials" not in session:    ## If not logged in
+        return redirect("authorize")    ## Start the login process
+    elif "user" in session:             ## If we are logged in
+                                        ## Return a customised index page
+        return "Session: " + session['user']['name']
+
+## Used by Google OAuth
+def credentials_to_dict(credentials):
+    return {'token': credentials.token,
+            'refresh_token': credentials.refresh_token,
+            'token_uri': credentials.token_uri,
+            'client_id': credentials.client_id,
+            'client_secret': credentials.client_secret,
+            'scopes': credentials.scopes,
+            'id_token': credentials.id_token}
+
+## Used by Google OAuth
+@app.route("/login")
+def login():
+    if "credentials" not in session:
+        return redirect("authorize")
+    else:
+        return redirect("/")
+
+## Used by Google OAuth
+@app.route("/authorize")
+def authorize():
+    # Intiiate login request
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(CLIENT_SECRETS_FILE, scopes=SCOPES)
+    flow.redirect_uri = url_for('oauth2callback', _external=True)
+    authorization_url, state = flow.authorization_url(access_type='offline', include_granted_scopes='true')
+    return redirect(authorization_url)
+
+## Used by Google OAuth
+@app.route("/oauth2callback")
+def oauth2callback():
+    # Receive an authorisation code from google
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(CLIENT_SECRETS_FILE, scopes=SCOPES)
+    flow.redirect_uri = url_for('oauth2callback', _external=True)
+    authorization_response = request.url
+    # Use authorisation code to request credentials from Google
+    flow.fetch_token(authorization_response=authorization_response)
+    credentials = flow.credentials
+    session['credentials'] = credentials_to_dict(credentials)
+    # Use the credentials to obtain user information and save it to the session
+    oauth2_client = build('oauth2','v2',credentials=credentials)
+    user_info= oauth2_client.userinfo().get().execute()
+    session['user'] = user_info
+    # Return to main page
+    return redirect("home")
+
+def main():   
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+    app.run(debug=True) 
+
+    # token_file_path = join(dirname(__file__), 'token-for-google.json')
+    # flow = InstalledAppFlow.from_client_secrets_file('client_secrets.json', scopes=[SCOPES])
+    
+    # flow.run_local_server()
+
+    # print(flow.credentials)
+
+    # flow = Flow.from_client_secrets_file('client_secrets.json', scopes=[SCOPES], redirect_uri='urn:ietf:wg:oauth:2.0:oob')
+    # auth_uri = flow.authorization_url()
+    # print(auth_uri)
+    # webbrowser.open(auth_uri[0])
+    # store = file.Storage(join(dirname(__file__), 'token-for-google.json'))
+    # creds = store.get()
+    # if not creds or creds.invalid:
+    #     flow = client.flow_from_clientsecrets(join(dirname(__file__), 'client_id.json', SCOPES))
+    #     creds = tools.run_flow(flow, store)
+    # google_photos = build('photoslibrary', 'v1', http=creds.authorize(Http()))
+
+    # day, month, year = ('0', '6', '2019')  # Day or month may be 0 => full month resp. year
+    # date_filter = [{"day": day, "month": month, "year": year}]  # No leading zeroes for day an month!
+    # nextpagetoken = 'Dummy'
+    # while nextpagetoken != '':
+    #     nextpagetoken = '' if nextpagetoken == 'Dummy' else nextpagetoken
+    #     results = google_photos.mediaItems().search(
+    #             body={"filters":  {"dateFilter": {"dates": [{"day": day, "month": month, "year": year}]}},
+    #                 "pageSize": 10, "pageToken": nextpagetoken}).execute()
+    #     # The default number of media items to return at a time is 25. The maximum pageSize is 100.
+    #     items = results.get('mediaItems', [])
+    #     nextpagetoken = results.get('nextPageToken', '')
+    #     for item in items:
+    #             print(f"{item['filename']} {item['mimeType']} '{item.get('description', '- -')}'"
+    #                     f" {item['mediaMetadata']['creationTime']}\nURL: {item['productUrl']}")
+
+if __name__ == "__main__":
+    main()
